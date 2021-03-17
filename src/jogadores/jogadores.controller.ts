a7be2751d75de45b2b757da6e6a9012abc117655
VALTERP <valter.paxi@ext.hdi.com.br>
2021-03-17 14:51:04 -0300

    Jogadores service created

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,17 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/cria-jogador.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('jogadores')
 export class JogadoresController {
 
+    constructor(private readonly jogadoresService: JogadoresService) {}
+
     @Post()
     async criarAtualizarJogador(
         @Body() criarJogadorDto: CriarJogadorDto) {
-        const { email, telefoneCelular } = criarJogadorDto
-            return JSON.stringify(`{
-                "email": ${email},
-                "telefoneCelular": ${telefoneCelular}
-            }`)
+        await this.jogadoresService.criarAtualizarJogador(criarJogadorDto)
     }
 
 }
